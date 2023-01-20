0924f77fd8362c64b4e2db6cd1d04f3e5d612305
nabilmersni <nabilmersni123@gmail.com>
2023-01-20 19:55:27 +0100

    finishing the header element. moving to the shoes slider

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-let loader = document.querySelector(".loader");
+const loader = document.querySelector(".loader");
 const nikeAnimation = document.querySelector(".header__nike__animation");
-
+const shoesColor = document.querySelector(".header__shoes__colors");
+let currentShoesColor;
 window.addEventListener("load", function () {
   setTimeout(function () {
-    // nikeAnimation.play();
+    nikeAnimation.play();
     loader.style.display = "none";
     document.body.style.overflowY = "visible";
   }, 2500);
+});
+
+shoesColor.addEventListener("click", function (e) {
+  if (e.target.type === "radio") {
+    currentShoesColor = e.target.value;
+    console.log(currentShoesColor);
+  }
 });
